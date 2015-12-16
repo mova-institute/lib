6cace9487603566bae8f28aeba1c44bdaf9aa832
Bohdan Moskalevskyi <msklvsk@icloud.com>
2015-12-16 20:09:16 +0200

    draft tag omohnymy finder

--- src/lang.ts
+++ src/lang.ts
@@ -9,3 +9,12 @@ export function ithGenerated<T>(generator: Iterator<T>, index: number) {
 	for (var cur = generator.next(); index >= 0 && !cur.done; --index, cur = generator.next());
 	return cur.value;
 }
+
+/*export function newOrPush<K, V>(map: Map<K, Array<V>>, key: K, val: V) {
+	if (map.has(key)) {
+		map.get(key).push(val);
+	}
+	else {
+		map.set(key, [val]);
+	}
+}*/
