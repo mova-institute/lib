e7b7e971c30001235414975ef603580b61b85844
Bohdan Moskalevskyi <msklvsk@icloud.com>
2015-12-21 10:50:06 +0200

    fix shev2viz

--- src/bin/misc/sheva2viz.ts
+++ src/bin/misc/sheva2viz.ts
@@ -5,22 +5,21 @@ let args = require('minimist')(process.argv.slice(2));
 
 let output = createWriteStream(args.o);
 
-let lexemes = new Map<string, Map<string, string>>();
+let lexemes = new Map<string, Array<[string, string]>>();
 for (let line of linesSync(args.i)) {
-	if (!line.includes(' ')) {
+  if (!line.includes(' ')) {
     let [form, lemma, tag] = line.split(',');
     if (!lexemes.has(lemma)) {
-      lexemes.set(lemma, new Map());
+      lexemes.set(lemma, []);
     }
-    lexemes.get(lemma).set(form, tag);
+    lexemes.get(lemma).push([form, tag]);
   }
 }
 
 for (let [lemma, lexeme] of lexemes) {
-  output.write(lemma + ' ' + lexeme.get(lemma) + '\n');
-  for (let [form, tag] of lexeme) {
-    if (form !== lemma) {
-      output.write('  ' + form + ' ' + tag + '\n');
-    }
+  output.write(lexeme[0][0] + ' ' + lexeme[0][1] + '\n');
+  for (let i = 1; i < lexeme.length; ++i) {
+    let [form, tag] = lexeme[i];
+    output.write('  ' + form + ' ' + tag + '\n');
   }
 }
