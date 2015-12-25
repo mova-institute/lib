d84f605362297f2b5460c3d337b76e2365a7c24d
Bohdan Moskalevskyi <msklvsk@icloud.com>
2015-12-25 23:16:58 +0200

    add CachedValue class

--- src/data_structures.ts
+++ src/data_structures.ts
@@ -16,77 +16,105 @@ export function linearizeWide(arrays: Array<Array<number>>, base = Uint32Array) 
 
 ////////////////////////////////////////////////////////////////////////////////
 export interface IMap<K, V> {
-	has(key: K): boolean;
-	get(key: K): V;
-	set(key: K, val: V): IMap<K, V>;
-	[Symbol.iterator]();
+  has(key: K): boolean;
+  get(key: K): V;
+  set(key: K, val: V): IMap<K, V>;
+  [Symbol.iterator]();
 }
 
 //------------------------------------------------------------------------------
 export class JsonCompareMap<K, V> implements IMap<K, V> {
-	map = new Map<string, [K, V]>();
+  map = new Map<string, [K, V]>();
 
-	constructor() {
-		this[Symbol.iterator]
-	}
+  constructor() {
+    this[Symbol.iterator]
+  }
 
-	has(key: K) {
-		return this.map.has(JSON.stringify(key));
-	}
+  has(key: K) {
+    return this.map.has(JSON.stringify(key));
+  }
 
-	get(key: K) {
-		return this.map.get(JSON.stringify(key))[1];
-	}
+  get(key: K) {
+    return this.map.get(JSON.stringify(key))[1];
+  }
 
-	set(key: K, val: V) {
-		this.map.set(JSON.stringify(key), [key, val]);
-		return this;
-	}
+  set(key: K, val: V) {
+    this.map.set(JSON.stringify(key), [key, val]);
+    return this;
+  }
 
-	[Symbol.iterator]() {
+  [Symbol.iterator]() {
     // todo
-	}
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 export class NumeratedSet<T> {  // todo move somewhere 
-	values = new Array<T>();
-	ids: IMap<T, number>;
+  values = new Array<T>();
+  ids: IMap<T, number>;
 
-	constructor(mapConstructor: { new (): IMap<T, number> } = Map) {
-		this.ids = new mapConstructor();
-	}
+  constructor(mapConstructor: { new (): IMap<T, number> } = Map) {
+    this.ids = new mapConstructor();
+  }
 
-	add(...vals: Array<T>) {
-		for (let val of vals) {
-			if (!this.ids.has(val)) {
-				this.ids.set(val, this.values.push(val) - 1);
-			}
-		}
+  add(...vals: Array<T>) {
+    for (let val of vals) {
+      if (!this.ids.has(val)) {
+        this.ids.set(val, this.values.push(val) - 1);
+      }
+    }
 
-		return this;
-	}
+    return this;
+  }
 
-	id(val: T) {
-		return this.ids.get(val);
-	}
+  id(val: T) {
+    return this.ids.get(val);
+  }
 
-	static fromUniqueArray(array: Array<any>) {
-		let toret = new NumeratedSet();
-		toret.values = array;
-		for (let i = 0; i < array.length; ++i) {
-			toret.ids.set(array[i], i);
-		}
-		
-		return toret;
-	}
-	
-	static fromSet(set: Set<any>) {
-		let toret = new NumeratedSet();
-		for (let val of set) {
-			toret.ids.set(val, toret.values.push(val) - 1);			
-		}
-		
-		return toret;
-	}
+  static fromUniqueArray(array: Array<any>) {
+    let toret = new NumeratedSet();
+    toret.values = array;
+    for (let i = 0; i < array.length; ++i) {
+      toret.ids.set(array[i], i);
+    }
+
+    return toret;
+  }
+
+  static fromSet(set: Set<any>) {
+    let toret = new NumeratedSet();
+    for (let val of set) {
+      toret.ids.set(val, toret.values.push(val) - 1);
+    }
+
+    return toret;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+export class CachedValue<T> {
+  private value: T;
+  private argsHash: string = null;
+
+  constructor(private calculator: (...args) => T) {
+
+  }
+
+  get(...args) {
+    let hash = JSON.stringify(args);
+    if (this.isInvalid() || (args && args.length && hash !== this.argsHash)) {
+      this.value = this.calculator(...args);
+    }
+    this.argsHash = hash;
+    
+    return this.value;
+  }
+
+  invalidate() {
+    this.argsHash = null;
+  }
+
+  private isInvalid() {
+    return this.argsHash === null;
+  }
 }
