7c32d1292c572a9bb9ae379f38f3565e7772f077
Bohdan Moskalevskyi <msklvsk@icloud.com>
2015-12-25 12:51:23 +0200

    add adj-ajp sheva vs rysin script

---
+++ src/bin/misc/compare-dranyj.ts
@@ -0,0 +1,38 @@
+//require('regenerator/runtime.js');  // todo
+import {linesSync} from '../../utils.node';
+
+let args = require('minimist')(process.argv.slice(2));
+
+let lemmataSheva = new Map<string, Set<string>>();
+for (let line of linesSync(args.sheva)) {
+  let [, lemma, tag] = line.split(' ');
+  if (!lemmataSheva.has(lemma)) {
+    lemmataSheva.set(lemma, new Set());
+  }
+  lemmataSheva.get(lemma).add(tag);
+}
+
+let lemmataRysin = new Map<string, Set<string>>();
+for (let line of linesSync(args.rysin)) {
+  if (!line.startsWith(' ')) {
+    let [lemma, tag] = line.split(' ');
+    if (!lemmataRysin.has(lemma)) {
+      lemmataRysin.set(lemma, new Set());
+    }
+    lemmataRysin.get(lemma).add(tag);
+  }
+}
+
+for (let [lemma, tags] of lemmataSheva) {
+  for (let tag of tags) {
+    if (tag && /^A[fo]/.test(tag) && !/^.........a/.test(tag)) {
+      let rysins = Array.from(lemmataRysin.get(lemma) || []);
+      if (rysins.length
+          && rysins.some(x => x.startsWith('Ap'))
+          && !rysins.some(x => /^A[fo]/.test(x))) {
+        console.log(lemma);
+        break;
+      }
+    }
+  }
+}
