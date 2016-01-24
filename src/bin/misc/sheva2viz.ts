df5433c040a16e6b3136b3cf8b82173334b92912
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-01-24 15:02:25 +0200

    fix sheva2viz lemma

--- src/bin/misc/sheva2viz.ts
+++ src/bin/misc/sheva2viz.ts
@@ -6,20 +6,25 @@ let args = require('minimist')(process.argv.slice(2));
 let output = createWriteStream(args.o);
 
 let lexemes = new Map<string, Array<[string, string]>>();
+let lemmaTags = new Map<string, string>();
 for (let line of linesSync(args.i)) {
   if (!line.includes(' ')) {
     let [form, lemma, tag] = line.split(',');
     if (!lexemes.has(lemma)) {
       lexemes.set(lemma, []);
     }
+    if (form === lemma && !lemmaTags.has(lemma)) {
+      lemmaTags.set(lemma, tag);
+    }
     lexemes.get(lemma).push([form, tag]);
   }
 }
 
 for (let [lemma, lexeme] of lexemes) {
-  output.write(lexeme[0][0] + ' ' + lexeme[0][1] + '\n');
-  for (let i = 1; i < lexeme.length; ++i) {
-    let [form, tag] = lexeme[i];
-    output.write('  ' + form + ' ' + tag + '\n');
+  output.write(lemma + ' ' + lemmaTags[lemma] + '\n');
+  for (let [form, tag] of lexeme) {
+    if (form !== lemma || tag !== lemmaTags[lemma]) {
+      output.write('  ' + form + ' ' + tag + '\n');
+    }
   }
 }
