92b4f066cde9019ef676a7bf51190d733f771c46
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-02 17:48:21 +0200

    fix sheva2viz

--- src/bin/misc/sheva2viz.ts
+++ src/bin/misc/sheva2viz.ts
@@ -1,4 +1,4 @@
-import {linesSync} from '../../utils.node';
+import {nonemptyLinesSync} from '../../utils.node';
 import {createWriteStream} from 'fs';
 
 let args = require('minimist')(process.argv.slice(2));
@@ -7,7 +7,7 @@ let output = createWriteStream(args.o);
 
 let lexemes = new Map<string, Array<[string, string]>>();
 let lemmaTags = new Map<string, string>();
-for (let line of linesSync(args.i)) {
+for (let line of nonemptyLinesSync(args.i)) {
   if (!line.includes(' ')) {
     let [form, lemma, tag] = line.split(',');
     if (!lexemes.has(lemma)) {
@@ -21,9 +21,9 @@ for (let line of linesSync(args.i)) {
 }
 
 for (let [lemma, lexeme] of lexemes) {
-  output.write(lemma + ' ' + lemmaTags[lemma] + '\n');
+  output.write(lemma + ' ' + lemmaTags.get(lemma) + '\n');
   for (let [form, tag] of lexeme) {
-    if (form !== lemma || tag !== lemmaTags[lemma]) {
+    if (form !== lemma || tag !== lemmaTags.get(lemma)) {
       output.write('  ' + form + ' ' + tag + '\n');
     }
   }
