8260aba3096ff5b3e9ac1766a1fb9bda76c056e4
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-06 22:35:22 +0200

    add es5 build config (used for mannotator prod)

--- gulpfile.js
+++ gulpfile.js
@@ -6,76 +6,77 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const del = require('del');
-const dirname = require('path').dirname;
 
-const PROJECR_ROOT = process.cwd();
-const dist = 'lib';
-const tsProjectFile = 'src/tsconfig.json';
 
-function swallowError (error) {
+const TS_PROJ_FILE = 'src/tsconfig.json';
+
+
+//------------------------------------------------------------------------------
+function swallowError(error) {
   console.log(error.toString());
   this.emit('end');
 }
 
-function nop() {}
 
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-gulp.task('typescript', [], () => {
-	let tsProject = ts.createProject(tsProjectFile, {
-		//typesctipt: require('typescript')
-	});
-
-	let tsResult = tsProject.src()
+//------------------------------------------------------------------------------
+function tsTask(target) {
+  let dest = target === 'es6' ? 'lib' : 'libes5';
+  let babelOpts = target === 'es6' ? undefined : { presets: ['es2015'] };
+  
+  let tsProject = ts.createProject(TS_PROJ_FILE);
+  let tsResult = tsProject.src()
 		//.pipe(sourcemaps.init({ debug: true }))
-		.pipe(ts(tsProject));
-	
-	tsResult.dts.pipe(gulp.dest('lib'));
-	
-	return tsResult.js
-		.pipe(babel(/*{presets: ['es2015']}*/).on('error', swallowError))
+    .pipe(ts(tsProject));
+
+  tsResult.dts.pipe(gulp.dest(dest));
+
+  return tsResult.js
+    .pipe(babel(babelOpts).on('error', swallowError))
 		// .pipe(sourcemaps.write('.', {
 		// 	includeContent: false,
 		// 	sourceRoot: PROJECR_ROOT + '/src'
 		// }))
-		.pipe(gulp.dest('lib'));
-});
-
-
+    .pipe(gulp.dest(dest));
+}
 
 ////////////////////////////////////////////////////////////////////////////////
+gulp.task('typescript', [], () => {
+  return tsTask('es6');
+});
+
+////////////////////////////////////////////////////////////////////////////////
+gulp.task('es5', [], () => {
+  return tsTask('es5');
+});
+
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('cleanup:dist', () => {
-	return del(['../mi-lib-dist/**/*'], { force: true });
+  return del(['../mi-lib-dist/**/*'], { force: true });
 });
+
 ////////////////////////////////////////////////////////////////////////////////
 gulp.task('typescript:dist', ['cleanup:dist'], () => {
-	let tsProject = ts.createProject(tsProjectFile, {
-		//typesctipt: require('typescript')
-	});
+  let tsProject = ts.createProject(TS_PROJ_FILE);
 
-	return tsProject.src()
-		.pipe(ts(tsProject))
-		.js
-		.pipe(babel({presets: ['es2015']}))
-		.pipe(uglify())
-		.pipe(gulp.dest('../mi-lib-dist/lib'));
+  return tsProject.src()
+    .pipe(ts(tsProject))
+    .js
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify())
+    .pipe(gulp.dest('../mi-lib-dist/lib'));
 });
 
 gulp.task('copy:dist', ['cleanup:dist'], () => {
-	return gulp.src(['bin/**', 'data/dict/**', 'package.json'], { base: '.' })
-		.pipe(gulp.dest('../mi-lib-dist/'));
+  return gulp.src(['bin/**', 'data/dict/**', 'package.json'], { base: '.' })
+    .pipe(gulp.dest('../mi-lib-dist/'));
 });
 
 
 
 
-gulp.task('build:dist', ['typescript:dist', 'copy:dist']);
-gulp.task('build', ['typescript']);
+gulp.task('dist', ['typescript:dist', 'copy:dist']);
+gulp.task('default', ['typescript']);
 
-gulp.task('develop', ['build'], () => {
-	gulp.watch(['src/**/*.ts', tsProjectFile], ['typescript']);
+gulp.task('develop', ['default'], () => {
+  gulp.watch(['src/**/*.ts', TS_PROJ_FILE], ['typescript']);
 });
-
-gulp.task('default', ['develop']);
