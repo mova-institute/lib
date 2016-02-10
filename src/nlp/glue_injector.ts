35ceab03c7ff12c7bada60b3dfa5aad9382f4ee4
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-10 21:33:21 +0200

    add semicolons

--- src/nlp/glue_injector.ts
+++ src/nlp/glue_injector.ts
@@ -1,8 +1,8 @@
-import {Transform} from 'stream'
-import {G, W, W_, PC} from './common_elements'
-import {NS} from '../xml/utils'
-import {SaxEventObject} from '../xml/sax_event_object'
-import {ELEMS_BREAKING_SENTENCE_NS, haveSpaceBetween} from '../nlp/utils'
+import {Transform} from 'stream';
+import {G, W, W_, PC} from './common_elements';
+import {NS} from '../xml/utils';
+import {SaxEventObject} from '../xml/sax_event_object';
+import {ELEMS_BREAKING_SENTENCE_NS, haveSpaceBetween} from '../nlp/utils';
 
 
 export class GlueInjector extends Transform {
