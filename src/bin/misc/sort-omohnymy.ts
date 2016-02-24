e6faa14ebf3fbb36b32a4802d72f4300aae30a9b
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-24 18:58:12 +0200

    refactor

--- src/bin/misc/sort-omohnymy.ts
+++ src/bin/misc/sort-omohnymy.ts
@@ -1,21 +1,21 @@
 import {filename2lxmlRootSync} from '../../utils.node';
-import {parseXmlString, Element} from 'libxmljs';
 import * as fs from 'fs';
 import {NS} from '../../xml/utils';
+import {LibxmlElement} from '../../xml/api/libxmljs_adapters';
 
 const args = require('minimist')(process.argv.slice(2));
 
-let doc = parseXmlString(fs.readFileSync(args._[0], 'utf8'));
+let doc = filename2lxmlRootSync(args._[0]);
 
 
-let words: Array<Element> = doc.find('//mi:w_', NS);
+let words: Array<LibxmlElement> = doc.xpath('//mi:w_', NS);
 
-for (let w_ of words) {
-  let interps = w_.find('./tei:w', NS).sort((a, b) =>
-    a.attr('ana').value().localeCompare(b.attr('ana').value()));
+for (let word of words) {
+  let interps = word.xpath('./tei:w', NS).sort((a, b) =>
+    a.getAttribute('ana').localeCompare(b.getAttribute('ana')));
   for (let w of interps) {
-    w_.addChild(w);
+    word.appendChild(w);
   }
 }
 
-process.stdout.write(doc.toString(), 'utf-8');
+process.stdout.write(doc.ownerDocument.serialize(), 'utf-8');
