bf9edfd72962bd22919445443965becf29bd3200
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-27 19:45:54 +0200

    develop universal converter

--- src/algo.ts
+++ src/algo.ts
@@ -5,56 +5,63 @@ export function numericCompare(a: number, b: number) {
 
 ////////////////////////////////////////////////////////////////////////////////
 export function uniqValuedMap2array(map) {
-	return Object.keys(map).sort((a, b) => {
-		return map[a] - map[b];
-	})
-} 
+  return Object.keys(map).sort((a, b) => {
+    return map[a] - map[b];
+  })
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 export function* findIndexwiseDiff(input: Array<any>) {
-	let maxLen = Math.max(...input.map(x => x.length));
-	let curDiffLen = 0;
-	for (let j = 0; j < maxLen; ++j) {
-		let cur = input[0][j];
-		for (let i = 1; i < input.length; ++i) {
-			if (input[i][j] !== cur) {
-				++curDiffLen;
-				break;
-			}
-			if (curDiffLen) {
-				yield [j - curDiffLen, curDiffLen];
-				curDiffLen = 0;
-			}
-		}
-	}
-	if (curDiffLen) {
-		yield [maxLen - curDiffLen, curDiffLen];
-	}
+  let maxLen = Math.max(...input.map(x => x.length));
+  let curDiffLen = 0;
+  for (let j = 0; j < maxLen; ++j) {
+    let cur = input[0][j];
+    for (let i = 1; i < input.length; ++i) {
+      if (input[i][j] !== cur) {
+        ++curDiffLen;
+        break;
+      }
+      if (curDiffLen) {
+        yield [j - curDiffLen, curDiffLen];
+        curDiffLen = 0;
+      }
+    }
+  }
+  if (curDiffLen) {
+    yield [maxLen - curDiffLen, curDiffLen];
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 export function longestCommonSubstring(strings: Array<string>) {  // naive
-	let ret = '';
-	if (strings.length) {
-		for (let i = 0; i < strings[0].length; ++i) {
-			for (let j = 0; j < strings[0].length - i + 1; ++j) {
-				let candidate = strings[0].substring(i, i + j);
-				if (j > ret.length && strings.every(x => x.indexOf(candidate) >= 0)) {
-					ret = candidate;
-				}
-			}
-		}
-	}
+  let ret = '';
+  if (strings.length) {
+    for (let i = 0; i < strings[0].length; ++i) {
+      for (let j = 0; j < strings[0].length - i + 1; ++j) {
+        let candidate = strings[0].substring(i, i + j);
+        if (j > ret.length && strings.every(x => x.indexOf(candidate) >= 0)) {
+          ret = candidate;
+        }
+      }
+    }
+  }
 
-	return ret;
+  return ret;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-export function indexTableByColumn(table: Object[], columnName: string) {
+export function indexTableByColumns(table: Object[], colNames: string[]) {
   let ret = new Map();
-  for (let row of table) {
-    ret.set(row[columnName], row);
-  }
   
+  for (let row of table) {
+    let cur = ret;
+    for (let i = 0, bound = colNames.length - 1; i < bound; ++i) {
+      let col = colNames[i];
+      let cell = row[col];
+      cur = cur.get(cell) || cur.set(cell, new Map()).get(cell);
+    }
+    cur.set(row[colNames[colNames.length - 1]], row);
+  }
+
   return ret;
 }
--- src/nlp/morph_tag.ts
+++ src/nlp/morph_tag.ts
@@ -1,4 +1,4 @@
-import {indexTableByColumn} from '../algo';
+import {indexTableByColumns} from '../algo';
 
 export enum Pos { // todo
   adjective,
@@ -80,7 +80,7 @@ export enum PronominalType {
 }
 
 export enum NumeralType {
-  
+
 }
 
 export enum Variant {
@@ -95,32 +95,32 @@ export enum Style {
 }
 
 
-const featureTable = [
-  { feat: 'animacy', featMi: Animacy, mi: Animacy.animate,   vesum: 'anim', mte: 'y' },
+export const featureTable = [
+  { feat: 'animacy', featMi: Animacy, mi: Animacy.animate, vesum: 'anim', mte: 'y' },
   { feat: 'animacy', featMi: Animacy, mi: Animacy.inanimate, vesum: 'inanim', mte: 'n' },
-  { feat: 'animacy', featMi: Animacy, mi: Animacy.animate,   vesum: 'ranim', mte: 'y' },  // ?
+  { feat: 'animacy', featMi: Animacy, mi: Animacy.animate, vesum: 'ranim', mte: 'y' },  // ?
   { feat: 'animacy', featMi: Animacy, mi: Animacy.inanimate, vesum: 'rinanim', mte: 'n' },  // ?
 
-  { feat: 'case', featMi: Case, mi: Case.nominative,    vesum: 'v_naz', mte: 'n' },
-  { feat: 'case', featMi: Case, mi: Case.genitive,      vesum: 'v_rod', mte: 'g' },
-  { feat: 'case', featMi: Case, mi: Case.dative,        vesum: 'v_dav', mte: 'd' },
-  { feat: 'case', featMi: Case, mi: Case.accusative,    vesum: 'v_zna', mte: 'a' },
-  { feat: 'case', featMi: Case, mi: Case.instrumental,  vesum: 'v_oru', mte: 'i' },
-  { feat: 'case', featMi: Case, mi: Case.locative,      vesum: 'v_mis', mte: 'l' },
-  { feat: 'case', featMi: Case, mi: Case.vocative,      vesum: 'v_kly', mte: 'v' },
+  { feat: 'case', featMi: Case, mi: Case.nominative, vesum: 'v_naz', mte: 'n' },
+  { feat: 'case', featMi: Case, mi: Case.genitive, vesum: 'v_rod', mte: 'g' },
+  { feat: 'case', featMi: Case, mi: Case.dative, vesum: 'v_dav', mte: 'd' },
+  { feat: 'case', featMi: Case, mi: Case.accusative, vesum: 'v_zna', mte: 'a' },
+  { feat: 'case', featMi: Case, mi: Case.instrumental, vesum: 'v_oru', mte: 'i' },
+  { feat: 'case', featMi: Case, mi: Case.locative, vesum: 'v_mis', mte: 'l' },
+  { feat: 'case', featMi: Case, mi: Case.vocative, vesum: 'v_kly', mte: 'v' },
   // { feat: 'case', featMi: Case, mi: Case, vesum: 'rv_naz', mte: 'n' }, 	// featMi: Case?
-  { feat: 'case', featMi: Case, mi: Case.genitive,      vesum: 'rv_rod', mte: 'g' }, 	// featMi: Case?
-  { feat: 'case', featMi: Case, mi: Case.dative,        vesum: 'rv_dav', mte: 'd' }, 	// featMi: Case?
-  { feat: 'case', featMi: Case, mi: Case.accusative,    vesum: 'rv_zna', mte: 'a' }, 	// featMi: Case?
-  { feat: 'case', featMi: Case, mi: Case.instrumental,  vesum: 'rv_oru', mte: 'i' }, 	// featMi: Case?
-  { feat: 'case', featMi: Case, mi: Case.locative,      vesum: 'rv_mis', mte: 'l' }, 	// featMi: Case?
+  { feat: 'case', featMi: Case, mi: Case.genitive, vesum: 'rv_rod', mte: 'g' }, 	// featMi: Case?
+  { feat: 'case', featMi: Case, mi: Case.dative, vesum: 'rv_dav', mte: 'd' }, 	// featMi: Case?
+  { feat: 'case', featMi: Case, mi: Case.accusative, vesum: 'rv_zna', mte: 'a' }, 	// featMi: Case?
+  { feat: 'case', featMi: Case, mi: Case.instrumental, vesum: 'rv_oru', mte: 'i' }, 	// featMi: Case?
+  { feat: 'case', featMi: Case, mi: Case.locative, vesum: 'rv_mis', mte: 'l' }, 	// featMi: Case?
 
   { feat: 'aspect', featMi: Aspect, mi: Aspect.imperfect, vesum: 'imperf', mte: 'p' },
   { feat: 'aspect', featMi: Aspect, mi: Aspect.perfect, vesum: 'perf', mte: 'e' },
 
-  { feat: 'tense', featMi: Tense, mi: Tense.past,    vesum: 'past', mte: 's' },
+  { feat: 'tense', featMi: Tense, mi: Tense.past, vesum: 'past', mte: 's' },
   { feat: 'tense', featMi: Tense, mi: Tense.present, vesum: 'pres', mte: 'p' },
-  { feat: 'tense', featMi: Tense, mi: Tense.future,  vesum: 'futr', mte: 'f' },
+  { feat: 'tense', featMi: Tense, mi: Tense.future, vesum: 'futr', mte: 'f' },
 
   { feat: 'verbForm', featMi: Mood, mi: Mood.imperative, vesum: 'impr', mte: 'm' },
   { feat: 'verbForm', featMi: Mood, mi: Mood.infinitive, vesum: 'inf', mte: 'n' },
@@ -145,11 +145,11 @@ const featureTable = [
   { feat: 'pronounType', featMi: PronominalType, mi: PronominalType.negative, vesum: 'neg', mte: 'z' },
   { feat: 'pronounType', featMi: PronominalType, mi: PronominalType.indefinite, vesum: 'ind', mte: 'i' },
   { feat: 'pronounType', featMi: PronominalType, mi: PronominalType.general, vesum: 'gen', mte: 'g' },
-  { feat: 'pronounType', featMi: null,           mi: null, vesum: 'def', mte: '?' },  // todo
+  { feat: 'pronounType', featMi: null, mi: null, vesum: 'def', mte: '?' },  // todo
   { feat: 'pronounType', featMi: PronominalType, mi: PronominalType.emphatic, vesum: 'emph', mte: 'h' },
 
-  { feat: 'сonjunctionType', featMi: null, vesum: 'coord', mte: 'c' },
-  { feat: 'сonjunctionType', featMi: null, vesum: 'subord', mte: 's' },
+  { feat: 'сonjunctionType', featMi: null, mi: Pos.coordinatingConjunction, vesum: 'coord', mte: 'c' },
+  { feat: 'сonjunctionType', featMi: null, mi: Pos.subordinatingConjunction, vesum: 'subord', mte: 's' },
 
   { feat: 'pos', featMi: null, mi: null, vesum: 'noun', mte: 'N' },  // todo
   { feat: 'pos', featMi: Pos, mi: Pos.pronoun, vesum: 'pron', mte: null },
@@ -166,6 +166,7 @@ const featureTable = [
   { feat: 'pos', featMi: Pos, mi: Pos.particle, vesum: 'part', mte: 'Q' },
   { feat: 'pos', featMi: Pos, mi: Pos.interjection, vesum: 'excl', mte: 'I' },
   { feat: 'pos', featMi: Pos, mi: Pos.numeral, vesum: 'numr', mte: 'M' },
+  // { feat: 'pos', featMi: Pos, mi: Pos.auxiliaryVerb, mte: 'M' },
 
   { feat: 'gender', featMi: Gender, mi: Gender.masculine, vesum: 'm', mte: 'm' },
   { feat: 'gender', featMi: Gender, mi: Gender.feminine, vesum: 'f', mte: 'f' },
@@ -183,7 +184,8 @@ const featureTable = [
 ];
 
 
-const mapVesum: Map<string, any> = indexTableByColumn(featureTable, 'vesum');
+const mapMi = indexTableByColumns(featureTable, ['featMi', 'mi']);
+const mapVesum: Map<string, any> = indexTableByColumns(featureTable, ['vesum']);
 
 
 
@@ -205,8 +207,32 @@ export class MorphTag {
     return featureTable;
   }
 
+  // expects altFlagsStr() output
   static fromVesum(value: string[]) {
+    let ret = new MorphTag();
 
+    for (let flag of value) {
+      let row = mapVesum.get(flag);
+      if (row) {
+        if (row.featMi) {
+          ret[row.feat] = row.mi;  // todo
+        }
+        else {
+          let feature = row.feat;
+          if (feature === 'сonjunctionType') {
+            ret.pos = row.mi;
+          }
+          else if (flag === 'noun') {
+
+          }
+        }
+      }
+      else {
+        console.error(`No mapping for vesum’s '${flag}'`);
+      }
+    }
+
+    return ret;
   }
 
   static fromUd(value: string) {
@@ -214,6 +240,46 @@ export class MorphTag {
   }
 
   toMte5() {
+    switch (this.pos) {
+      case Pos.noun:
+      case Pos.properNoun: {
+        return 'N'
+          + ''
+      }
+      
+      case Pos.verb:
+      case Pos.auxiliaryVerb: {
+        let ret = 'V'
+          + (this.pos === Pos.verb ? 'm' : 'a')
+        // + ;
+        return ret;
+      }
+      
+      case Pos.numeral:
+        return 'Ml'
+          + ''
+      
+      case Pos.adverb:
+        return 'R'
+          + '';
+      
+      case Pos.adposition:
+        return 'Sp'
+          + '-'  // todo: formation
+          + mapMi.get(Case).get(this.case);
+          
+      case Pos.coordinatingConjunction:
+      case Pos.coordinatingConjunction:
+        return 'C'
+          + (this.pos === Pos.coordinatingConjunction ? 'c' : 's')
+          + '';  // todo: simple/compound
+          
+      case Pos.interjection:
+        return 'I';
+        
+      case Pos.particle:
+        return 'Q';
+    }
 
   }
 
@@ -227,7 +293,7 @@ export class MorphTag {
 }
 
 
-// Expands dict_corp_viz.txt tag into an array of unambiguous morph interpretations.
+// Expands dict_corp_viz.txt tag into an array of unambiguous morph interpretations
 ////////////////////////////////////////////////////////////////////////////////
 export function expandVesumTag(value: string) {
   let [mainFlagsStr, altFlagsStr] = value.split(/:&_|:&(?=adjp)/);  // adj:m:v_zna:rinanim:&adjp:pasv:imperf
@@ -255,13 +321,29 @@ export function expandVesumTag(value: string) {
   else {
     ret.push(mainFlagsArray);
   }
-  
+
   if (altFlagsStr) {
     let altFlagArray = altFlagsStr.split(':');
     for (let i = 0, length = ret.length; i < length; ++i) {
       ret.push([...ret[i], '&' + altFlagArray[0], ...altFlagArray.slice(1)]);
     }
   }
-  
+
   return ret;
 }
+
+
+
+
+
+
+
+/*
+
+todo:
+- strings to symbol/enum
+
+
+vesum tests: conjunction always has type
+
+*/
