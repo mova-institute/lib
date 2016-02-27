e807b50fc7ea0be6b6848b3795d681a19514a0e3
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-02-27 16:46:11 +0200

    add expandVesumTag

--- src/nlp/morph_tag.ts
+++ src/nlp/morph_tag.ts
@@ -183,7 +183,7 @@ const featureTable = [
 ];
 
 
-const mapVesum: Map<string, Object> = indexTableByColumn(featureTable, 'vesum');
+const mapVesum: Map<string, any> = indexTableByColumn(featureTable, 'vesum');
 
 
 
@@ -200,28 +200,68 @@ export class MorphTag {
   animacy: Animacy;
   gender: Gender;
   degree: Degree;
-  
+
   static fromMte5(value: string): any {
     return featureTable;
   }
-  
+
   static fromVesum(value: string[]) {
-    
+
   }
-  
+
   static fromUd(value: string) {
-    
+
   }
-  
+
   toMte5() {
-    
+
   }
-  
+
   toVesum() {
-    
+
+  }
+
+  toUd() {
+
+  }
+}
+
+
+// Expands dict_corp_viz.txt tag into an array of unambiguous morph interpretations.
+////////////////////////////////////////////////////////////////////////////////
+export function expandVesumTag(value: string) {
+  let [mainFlagsStr, altFlagsStr] = value.split(/:&_|:&(?=adjp)/);  // adj:m:v_zna:rinanim:&adjp:pasv:imperf
+
+  let mainFlagsArray = mainFlagsStr.split(':');
+  let mainFlags = new Set(mainFlagsArray);
+  let arrayFeature = [];
+  for (let flag of mainFlagsArray) {
+    if (mapVesum.has(flag)) {
+      let feature = mapVesum.get(flag).feat;
+      if (mainFlagsArray[0] === 'prep' && feature === 'case' || feature === 'pronounType') {
+        arrayFeature.push(flag);
+        mainFlags.delete(flag);
+      }
+    }
+  }
+
+  let ret = new Array<Array<string>>();
+  if (arrayFeature.length) {
+    let base = Array.from(mainFlags);
+    for (let flag of arrayFeature) {
+      ret.push([...base, flag]);
+    }
+  }
+  else {
+    ret.push(mainFlagsArray);
   }
   
-  toUd() {
-    
+  if (altFlagsStr) {
+    let altFlagArray = altFlagsStr.split(':');
+    for (let i = 0, length = ret.length; i < length; ++i) {
+      ret.push([...ret[i], '&' + altFlagArray[0], ...altFlagArray.slice(1)]);
+    }
   }
+  
+  return ret;
 }
