5382724814b8ab8a51fc6aeaa31d6626e03fd9df
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-03-22 19:22:28 +0200

    add fromMte test

--- src/tests.ts
+++ src/tests.ts
@@ -9,7 +9,7 @@ const debug = require('debug')('testo');
 ////////////////////////////////////////////////////////////////////////////////
 export function findDuplicateFeatures(fileStr: string) {
   let ret = new Set<string>();
-  
+
   let skip = new Set(['&_adjp', '&_numr', 'v-u', 'dimin']);
   for (let {tag} of iterateDictCorpVizLines(fileStr.split('\n'))) {
     let features = new Set<string>();
@@ -24,11 +24,44 @@ export function findDuplicateFeatures(fileStr: string) {
       features.add(feature);
     }
   }
-  
+
   console.log(ret);
   return ret;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+export function testMte2Vesum(fileStr: string) {
+  let lines = fileStr.split('\n');
+  for (let {form, tag, lemma, lemmaTag, isLemma, lineNum} of iterateDictCorpVizLines(lines)) {
+    if (tag.includes('transl')
+      || tag.includes('insert')
+      || tag.includes('predic')
+      || tag.includes('bad')) {
+      continue;
+    }
+    try {
+      var mte1 = rysin2multext(lemma, lemmaTag, form, tag)[0];
+      var fromMte = MorphTag.fromMte(mte1);
+      var vesum2 = fromMte.toVesumStr();
+      var mte2 = rysin2multext(lemma, lemmaTag, form, vesum2)[0];
+      if (mte1 !== mte2) {
+        // throw new Error(`${tag} !== ${toMte}`);
+        console.error(`${tag}\n${vesum2}\n${mte1}\n${mte2}   for ${form} ${vesum2}\n`);
+      }
+      mte1 = fromMte = vesum2 = mte2 = null;
+    }
+    catch (e) {
+      console.error({ form, tag, mte1, vesum2, mte2, lineNum });
+      if (e.message.startsWith('Unma')) {
+        throw e;
+        //continue;
+      }
+      else {
+        throw e;
+      }
+    }
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 export function testConverter(fileStr: string) {
