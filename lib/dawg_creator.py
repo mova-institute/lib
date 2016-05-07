5a4f21869aca998ae83c1b852932ee60fb2725e8
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-05-07 19:50:43 +0300

    format

--- lib/dawg_creator.py
+++ lib/dawg_creator.py
@@ -7,12 +7,12 @@ import dawg
 base = sys.argv[1]
 
 with open(base + '/words.dawg.json') as file:
-    json_data = json.load(file)
-    dawg_data = []
-    for word in json_data:
-        dawg_data.append(
-                (word[0], (word[1], word[2]))
-        )
+  json_data = json.load(file)
+  dawg_data = []
+  for word in json_data:
+    dawg_data.append(
+      (word[0], (word[1], word[2]))
+    )
 
-    dwg = dawg.RecordDAWG('>HH', dawg_data)
-    dwg.write(open(base + '/words.dawg', 'wb'))
+  dwg = dawg.RecordDAWG('>HH', dawg_data)
+  dwg.write(open(base + '/words.dawg', 'wb'))
--- lib/dawg_creator_mi_encoding.py
+++ lib/dawg_creator_mi_encoding.py
@@ -4,12 +4,11 @@ import sys
 import array
 import dawg
 
-
 base = sys.argv[1]
 
 dawg_data = []
 for l in open(base + '/words.dawg.lst'):
-    dawg_data.append(array.array('B', [int(x) for x in l.split()]).tobytes())
+  dawg_data.append(array.array('B', [int(x) for x in l.split()]).tobytes())
 
 dwg = dawg.CompletionDAWG(dawg_data)
 dwg.write(open(base + '/words.dawg', 'wb'))
