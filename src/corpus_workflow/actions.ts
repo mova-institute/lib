e9718ae356ddbd80cce75b7e440165c43295d5f0
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-05-25 14:11:18 +0300

    add experimental corpus workflow server logic

---
+++ src/corpus_workflow/actions.ts
@@ -0,0 +1,20 @@
+import {createMorphAnalyserSync} from '../nlp/morph_analyzer/factories.node';
+import {string2lxmlRoot} from '../utils.node';
+import {tokenizeTei} from '../nlp/utils';
+import {getLibRootRelative} from '../path.node';
+
+let morphAnalyzer = createMorphAnalyserSync(getLibRootRelative('..', 'data', 'dict', 'vesum'));
+
+////////////////////////////////////////////////////////////////////////////////
+export function tokenize(input: {xmlstr: string}) {
+  let root = string2lxmlRoot(input.xmlstr);
+  let ret = tokenizeTei(root, morphAnalyzer);
+
+  return ret.document.serialize();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+export function findUnknownWords(input: { words: string[] }) {
+  // console.error(words);
+  return input.words.map(x => morphAnalyzer.dictHas(x));
+}
---
+++ src/corpus_workflow/server.ts
@@ -0,0 +1,31 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as Actions from './actions';
+const args = require('minimist')(process.argv.slice(2));
+
+
+let app = express();
+app.disable('x-powered-by');
+app.disable('etag');
+app.set('json spaces', 2);
+app.use(bodyParser.json({ limit: '50mb' }));
+
+
+app.all(/\/(\w+)(\/.*)?/, (req: express.Request, res: express.Response) => {
+  // console.log(req.params);
+  let actionName = req.params[0];
+  if (actionName in Actions) {
+    let input = Object.assign({}, req.body, req.query);
+    // console.error(req.body);
+    let output = Actions[actionName](input);
+    res.json({
+      data: output,
+    });
+    // res.json(output);
+  }
+  else {
+    throw new Error();
+  }
+});
+
+app.listen(args.p || args.port || 8888);
