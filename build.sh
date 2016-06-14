ae2dc5816112d606db352524a9628da1c3930a7d
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-06-14 22:19:07 +0300

    update build process

---
+++ build.sh
@@ -0,0 +1,1 @@
+cd ../dist && npm i --prod --ignore-scripts ../mi-lib ../dawgjs ../xmlapi ../xmlapi-libxmljs
--- deploy.sh
+++ deploy.sh
@@ -14,11 +14,11 @@ elif [ $1 == "annotator" ]
     if [[ -z $2 ]]
       then
         echo "Running dry-run"
-        rsync --dry-run -rlpcgoD --force --delete            -i -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST | pcregrep '^[<>*]' || exit 1
+        rsync --dry-run -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build'            -i -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST | pcregrep '^[<>*]' || exit 1
     elif [ $2 == "go" ]
       then
         echo "Running actual deploy"
-        rsync           -rlpcgoD --force --delete --progress    -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST                    || exit 1
+        rsync           -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build' --progress    -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST                    || exit 1
         ssh -i $SSH_KEY $REMOTE "cd $DEST && pm2 restart annotator" || exit 1
     else
       echo $ERRORSTRING;
