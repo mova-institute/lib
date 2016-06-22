02bd02c5049addfb46ee1e8e36d1079bbd717467
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-06-22 15:01:19 +0300

    rename beforedash

--- src/nlp/morph_tag.ts
+++ src/nlp/morph_tag.ts
@@ -189,7 +189,7 @@ export enum Possesive {
 }
 export enum ParadigmOmohnym { }
 export enum SemanticOmohnym { }
-export enum Predash {
+export enum Beforedash {
   yes,
 }
 export enum Auto {
@@ -391,7 +391,7 @@ export class MorphTag {
   private static otherFlagsAllowed = new Set([
     'xp1', 'xp2', 'xp3', 'xp4', 'xp5', 'xp6', 'xp7',
     'xv1', 'xv2', 'xv3', 'xv4', 'xv5', 'xv6', 'xv7',
-    'nv', 'alt', 'bad', 'abbr', 'v-u', 'dimin', 'transl', 'mock', 'odd', 'predash',
+    'nv', 'alt', 'bad', 'abbr', 'v-u', 'dimin', 'transl', 'mock', 'odd', 'beforedash',
   ]);
 
 
