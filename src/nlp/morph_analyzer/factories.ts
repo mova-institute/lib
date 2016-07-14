6d550585e0bd0e09ba239d51488883e3fed52841
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-07-15 01:30:54 +0300

    fix createMorphAnalyser() for the web

--- src/nlp/morph_analyzer/factories.ts
+++ src/nlp/morph_analyzer/factories.ts
@@ -1,6 +1,8 @@
 import { MorphAnalyzer } from '../morph_analyzer/morph_analyzer';
 import { WordDawgPayload } from '../dictionary/word_dawg_payload';
-// import { readStringMapDawgSync } from 'dawgjs/factories';
+import { Dictionary } from '../dictionary/dictionary';
+
+import { readStringMapDawg } from 'dawgjs/factories';
 
 
 export function createMorphAnalyser(
@@ -9,20 +11,17 @@ export function createMorphAnalyser(
   suffixes: string[],
   tags: string[]) {
 
-  throw new Error('Not implemented');
-  // let paradigms = new Array<Uint16Array>();
-  // let paradigmsView = new DataView(paradigmsBuf);
-  // let curByte = 0;
-  // while (curByte < paradigmsBuf.byteLength) {
-  //   let paradigmLen = paradigmsView.getUint16(curByte, true) * 3;
-  //   curByte += 2;
-  //   paradigms.push(new Uint16Array(paradigmsBuf, curByte, paradigmLen));
-  //   curByte += paradigmLen * 2;
-  // }
-  // console.log('fuuuuuuuuisdjflksbflksdbfkshbd');
+  let paradigms = new Array<Uint16Array>();
+  let paradigmsView = new DataView(paradigmsBuf);
+  let curByte = 0;
+  while (curByte < paradigmsBuf.byteLength) {
+    let paradigmLen = paradigmsView.getUint16(curByte, true) * 3;
+    curByte += 2;
+    paradigms.push(new Uint16Array(paradigmsBuf, curByte, paradigmLen));
+    curByte += paradigmLen * 2;
+  }
 
-
-  // let dawg = createStringMapDawg(wordsBuf, WordDawgPayload.create);
-  // let ret = new MorphAnalyzer(dawg, paradigms, suffixes, tags, 'num', 'x');  // todo
-  // return ret;
+  let wordsDawg = readStringMapDawg(wordsBuf, WordDawgPayload.create, 1, true);
+  let dictionary = new Dictionary(wordsDawg, paradigms, suffixes, tags);
+  return new MorphAnalyzer(dictionary, 'numr', 'x:foreign', 'x');
 }
