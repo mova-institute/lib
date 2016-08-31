29955bb8931aabfa2a177695b4600d28f06d6767
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-08-31 21:17:40 +0300

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-
-SSH_KEY="$HOME/.ssh/mova.institute.key"
-REMOTE="ubuntu@mova.institute"
+REMOTE="msklvsk@mova.institute"
 DEST="/opt/node/annotator"
 SOURCE="../dist/*"
 ERRORSTRING="Wrong params"
@@ -14,12 +12,12 @@ elif [ $1 == "annotator" ]
     if [[ -z $2 ]]
       then
         echo "Running dry-run"
-        rsync --dry-run -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build'            -i -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST | pcregrep '^[<>*]' || exit 1
+        rsync --dry-run -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build'            -i -e ssh $SOURCE $REMOTE:$DEST | pcregrep '^[<>*]' || exit 1
     elif [ $2 == "go" ]
       then
         echo "Running actual deploy"
-        rsync           -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build' --progress    -e "ssh -i $SSH_KEY" $SOURCE $REMOTE:$DEST                    || exit 1
-        ssh -i $SSH_KEY $REMOTE "cd $DEST && pm2 restart annotator" || exit 1
+        rsync           -rlpcgoD --force --delete --exclude 'node_modules/libxmljs/build' --progress    -e ssh $SOURCE $REMOTE:$DEST                    || exit 1
+        ssh $REMOTE "cd $DEST && pm2 restart annotator" || exit 1
     else
       echo $ERRORSTRING;
       exit 1
