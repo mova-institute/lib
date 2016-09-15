07f1014e885689be78e3d7ba28692a6eb3323e5f
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-09-15 22:43:34 +0300

    cleanup [trashing]

--- src/bin/enumerate-words.ts
+++
@@ -1,9 +0,0 @@
-import { ioArgsPlain } from '../cli_utils'
-import { stream2lxmlRoot } from '../utils.node'
-import { enumerateWords } from '../nlp/utils'
-
-ioArgsPlain(async (input, output) => {
-  let root = await stream2lxmlRoot(input)
-  enumerateWords(root)
-  output.write(root.document().serialize())
-})
