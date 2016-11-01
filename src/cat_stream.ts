2c32336269e7c5030844627b1c6c4609255f0fe4
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-11-01 16:35:07 +0200

    implement CatStream

---
+++ src/cat_stream.ts
@@ -0,0 +1,35 @@
+import { createReadStream, ReadStream } from 'fs'
+import { Readable, ReadableOptions } from 'stream'
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+export class CatStream extends Readable {
+  private currentReadStream: ReadStream
+  private iterator: Iterator<string>
+
+  constructor(paths: Iterable<string>, options?: ReadableOptions) {
+    super(options)
+    this.iterator = paths[Symbol.iterator]()
+    this.streamNext()
+  }
+
+  _read(size?: number) {
+    this.currentReadStream.resume()
+  }
+
+  private streamNext() {
+    let {value, done} = this.iterator.next()
+    if (done) {
+      this.push(null)
+    } else {
+      this.currentReadStream = createReadStream(value)
+        .on('data', chunk => {
+          if (!this.push(chunk)) {
+            this.currentReadStream.pause()
+          }
+        })
+        .once('end', () => this.streamNext())
+    }
+  }
+}
