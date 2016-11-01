5bddb05b8ddf5b2d445c9769610a3935ecda215e
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-11-02 01:44:12 +0200

    add remorph-conllu script

---
+++ src/nlp/ud/remorphconllu.ts
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+import { forEachLine } from '../../utils.node'
+import { canBeConlluLine } from './utils'
+import { toUdString } from './tagset'
+import { MorphInterp } from '../../nlp/morph_interp'
+
+if (require.main === module) {
+  main()
+}
+
+function main() {
+  forEachLine(process.stdin, line => {
+    if (canBeConlluLine(line)) {
+      let cols = line.split('\t')
+      let interp = MorphInterp.fromVesumStr(cols[4], cols[2])
+      cols[5] = toUdString(interp) || '_'
+      process.stdout.write(cols.join('\t') + '\n')
+    } else {
+      process.stdout.write(line + '\n')
+    }
+  })
+}
