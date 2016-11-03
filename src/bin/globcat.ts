0b627d648f0fa41180e99ed21c070ed3475ce2f1
Bohdan Moskalevskyi <msklvsk@icloud.com>
2016-11-03 20:52:42 +0200

    add globcat

---
+++ src/bin/globcat.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import * as glob from 'glob'
+
+import { allLinesFromStdin, ignorePipeErrors } from '../utils.node'
+import { CatStream } from '../cat_stream'
+import { mu } from '../mu'
+
+
+
+if (require.main === module) {
+  main()
+}
+
+async function main() {
+  let globs = (await allLinesFromStdin())
+  let files = mu(globs).map(x => glob.sync(x)).flatten()
+  let stream = new CatStream(files)
+  stream.pipe(process.stdout)
+  ignorePipeErrors()
+}
