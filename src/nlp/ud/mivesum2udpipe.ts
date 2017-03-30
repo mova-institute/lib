639e973d411dd86b10e6391427146f0839a80fb6
Bohdan Moskalevskyi <msklvsk@icloud.com>
2017-03-30 13:03:29 +0300

    add mivesum2Udpipe

---
+++ src/nlp/ud/mivesum2udpipe.ts
@@ -0,0 +1,27 @@
+import { iterateDictCorpVizLines } from '../vesum_utils'
+import { toUd, udFeatures2conlluString } from './tagset'
+import { MorphInterp } from '../morph_interp'
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+export function* mivesum2Udpipe(lines: Iterable<string>) {
+  for (let { form, tag, lemma } of iterateDictCorpVizLines(lines)) {
+    let interp = MorphInterp.fromVesumStr(tag, lemma)
+    try {
+      let udTag = toUd(interp)
+
+      yield [
+        form,
+        lemma,
+        udTag.pos,
+        '_',  // no xpostag
+        udFeatures2conlluString(udTag.features) || '_',
+      ].join('\t')
+    } catch (e) {
+      if (!e.message.startsWith('Emphatic pronoun conversion is not implemented')) {
+        throw e
+      }
+    }
+  }
+}
