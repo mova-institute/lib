b597ecd5db99a20393d4f2b4388b48074d1ba609
Bohdan Moskalevskyi <msklvsk@icloud.com>
2017-10-07 19:16:46 +0300

    cleanup

--- misc/setup_aws_tf_env.sh
+++ misc/setup_aws_tf_env.sh
@@ -54,7 +54,6 @@ git clone https://github.com/tensorflow/tensorflow &&
 cd tensorflow &&
 git checkout r1.3 &&
 export TF_NEED_CUDA=1
-# (TF_NEED_MKL not supported with cuda)
 yes '' | ./configure &&
 bazel build --config=opt --config=cuda //tensorflow/tools/pip_package:build_pip_package &&
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg &&
