7cae2467acfa13068845a804a9a322a5380ad9f9
Bohdan Moskalevskyi <msklvsk@icloud.com>
2017-12-11 01:09:55 +0200

    switch to registry file generation

--- src/corpus-pipeline/registry_file_builder.ts
+++ src/corpus-pipeline/registry_file_builder.ts
@@ -1,7 +1,18 @@
-NAME "золотий"
+///////////////////////////////////////////////////////////////////////////////
+export interface RegistryFileParams {
+  // name: string
+  title: string
+  hasGaps: boolean
+}
+
+////////////////////////////////////////////////////////////////////////////////
+export function generateRegistryFile(params: RegistryFileParams) {
+  let corpus = `
+
+NAME "${params.title}"
 #INFO "Корпус української (випробовування)"   # todo: say mova intitute some day
 INFOHREF "https://mova.institute/corpus"
-MAINTAINER "corpus@mova.institute"
+MAINTAINER "org@mova.institute"
 TAGSETDOC "http://universaldependencies.org"    # todo
 
 
@@ -349,13 +360,18 @@ STRUCTURE s {
 STRUCTURE g {
   DISPLAYTAG 0
   DISPLAYBEGIN "_EMPTY_"
-}
+}`
+  if (params.hasGaps) {
+    corpus += `
 STRUCTURE gap {
   LABEL "пропуск"
   ATTRIBUTE type {
     LABEL "тип"
   }
-}
+}`
+  }
+  corpus += `
+
 
 
 ################################################################################
@@ -378,7 +394,69 @@ DEFAULTATTR word
 # todo ATTRDOC, ATTRDOCLABEL,
 
 FULLREF "doc.title,doc.author,doc.original_author,doc.date,doc.type,doc.domain,doc.comment,doc.wordcount,s.id,doc.url"
-STRUCTATTRLIST "doc.reference_title,doc.author,doc.date,doc.type"
+STRUCTATTRLIST "doc.title,doc.author,doc.date,doc.type"
 SUBCORPATTRS "doc.title,doc.author|doc.date,doc.type"
 #FREQTTATTRS ""
 WPOSLIST ",іменник,noun|propn,дієслово,verb,прикметник,adj,прислівник,adv,прийменник,adp,сполучник,cconj|sconj,числівник,num,частка,part,вигук,intj,розділовий,punct,залишок,x"
+`
+
+
+  corpus = corpus.trim()
+  let subcorpus = `
+
+*FREQLISTATTRS word lc lemma lemma_lc tag tag2
+
+=до30ті
+  -CQL-
+  <doc date="[0-9]{4}.*" & date<="1932">
+
+  `.trim()
+
+  return { corpus, subcorpus }
+}
+
+
+const uiSettings = {
+  uilang: 'uk',
+  attrs: 'word',
+  copy_icon: '0',
+  ctxattrs: 'word',
+  gdex_enabled: '0',
+  gdexcnt: '100',
+  line_numbers: '1',
+  multiple_copy: '0',
+  pagesize: '20',
+  refs: '=doc.reference_title',
+  refs_up: '0',
+  select_lines: '1',
+  shorten_refs: '1',
+  show_gdex_scores: '0',
+  structs: 'doc,g',
+  use_noflash: '0',
+}
+
+
+/*
+
+
+
+
+reference_title
+title назва
+date дата
+author автор
+original_author автор первотвору
+url посилання
+disamb уоднозначнення    жодного|часткове-правила|руками-Політехніка|руками-стандарт
+type тип    художня проза|поезія|публіцистика|закон (НПА)||
+
+domain галузь спорт|економіка|мистецтво|історія|
+
+хххх    оповідання|стаття|роман|
+
+wordcount
+comment
+
+// proofread
+
+*/
