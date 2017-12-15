8f52b2815015e8b7ca60a01a5a6419234d14b7df
Bohdan Moskalevskyi <msklvsk@icloud.com>
2017-12-15 20:45:25 +0200

    enhance wikipedia filtering

--- src/corpus-pipeline/extractors/wikipedia.ts
+++ src/corpus-pipeline/extractors/wikipedia.ts
@@ -7,7 +7,7 @@ export function* streamDocs(wikiExtractorFileStr: string) {
   for (let line of wikiExtractorFileStr.trim().split('\n')) {
     let { url, text, title } = JSON.parse(line)
     // workaround https://github.com/attardi/wikiextractor/issues/53
-    text = text.replace(/\s*(\(\)|formula_\d+)/g, '').trim()
+    text = text.replace(/\s*(\(\)|\([,;][^)]*\)|formula_\d+)/g, '').trim()
     if (text) {
       let paragraphs = text.split(/\n{2,}/).map(x => x.replace(/\n+/g, ' '))
       yield { url, title, paragraphs } as CorpusDoc
