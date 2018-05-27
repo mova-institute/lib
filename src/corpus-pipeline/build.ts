267b9f97998eccab6e8893f57927a948403f3642
Bohdan Moskalevskyi <msklvsk@icloud.com>
2018-05-27 20:17:11 +0300

    refactor

--- src/corpus-pipeline/build.ts
+++ src/corpus-pipeline/build.ts
@@ -2,12 +2,8 @@
 
 import { CorpusDoc } from './doc_meta'
 import { createMorphAnalyzerSync } from '../nlp/morph_analyzer/factories.node'
-import { writeFileSyncMkdirp, parseJsonFileSync } from '../utils.node'
 import { trimExtension, zerofill, toFloorPercent } from '../string_utils'
 import { mu } from '../mu'
-import { AsyncTaskRunner } from '../lib/async_task_runner'
-import { UdpipeApiClient } from '../nlp/ud/udpipe_api_client'
-import { conlluStrAndMeta2vertical } from './tovert'
 
 import { sync as globSync } from 'glob'
 import * as minimist from 'minimist'
@@ -27,62 +23,49 @@ interface SpecificExtractor {
 
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 function main(args: Args) {
-  let outDir = getOutDir(args)
+  let outDir = join(args.workspace, args.part)
 
-  if (args.stage === 'extract') {
-    let analyzer = createMorphAnalyzerSync().setExpandAdjectivesAsNouns(false).setKeepN2adj(true)
+  let analyzer = createMorphAnalyzerSync().setExpandAdjectivesAsNouns(false).setKeepN2adj(true)
 
-    let inputFiles = globInforming(args.inputRoot, args.inputGlob)
-    if (args.part === 'chtyvo') {  // todo
-      inputFiles = mu(inputFiles)
-        .map(x => trimExtension(x))
-        .filter(x => !x.endsWith('.meta'))
-        .unique()
-        .toArray()
-    }
-
-    let specificExtractor = require(`./extractors/${args.part}`) as SpecificExtractor
-    let docCounter = 0
-
-    for (let [fileI, filePath] of inputFiles.entries()) {
-      let tolog = `extracted ${fileI} files (${toFloorPercent(fileI, inputFiles.length)}%), ${docCounter} docs, doing ${filePath}`
-
-      let relPath = path.relative(args.inputRoot, filePath)
-      if (specificExtractor.extract) {
-        if (getMetaParaPaths(outDir, relPath).some(x => !fs.existsSync(x))) {
-          console.log(tolog)
-          let fileStr = fs.readFileSync(filePath, 'utf8')
-          processDoc(specificExtractor.extract(fileStr), outDir, relPath, analyzer)
-        }
-        ++docCounter
-      } else if (specificExtractor.streamDocs) {
-        if (fs.existsSync(join(outDir, 'meta', relPath))) {
-          continue
-        }
-        console.log(tolog)
-        let inputStr = args.part === 'chtyvo' ? filePath : fs.readFileSync(filePath, 'utf8')
-        let i = 0
-        for (let doc of specificExtractor.streamDocs(inputStr)) {
-          let docId = join(relPath, zerofill(i++, 4))
-          processDoc(doc, outDir, docId, analyzer)
-        }
-        docCounter += i
-      } else {
-        throw new Error(`No extractor for ${args.part}`)
-      }
-    }
-  } else if (args.stage === 'udpipe') {
-    doUdpipeStage(args)
-  } else if (args.stage === 'vertical') {
-    doVerticalStage(args)
-  } else {
-    throw new Error('Unknown stage')
+  let inputFiles = globInforming(args.inputRoot, args.inputGlob)
+  if (args.part === 'chtyvo') {  // todo
+    inputFiles = mu(inputFiles)
+      .map(x => trimExtension(x))
+      .filter(x => !x.endsWith('.meta'))
+      .unique()
+      .toArray()
   }
-}
 
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-function getOutDir(args: Args) {
-  return join(args.workspace, args.out || args.part)
+  let specificExtractor = require(`./extractors/${args.part}`) as SpecificExtractor
+  let docCounter = 0
+
+  for (let [fileI, filePath] of inputFiles.entries()) {
+    let tolog = `extracted ${fileI} files (${toFloorPercent(fileI, inputFiles.length)}%), ${docCounter} docs, doing ${filePath}`
+
+    let relPath = path.relative(args.inputRoot, filePath)
+    if (specificExtractor.extract) {
+      if (getMetaParaPaths(outDir, relPath).some(x => !fs.existsSync(x))) {
+        console.log(tolog)
+        let fileStr = fs.readFileSync(filePath, 'utf8')
+        processDoc(specificExtractor.extract(fileStr), outDir, relPath, analyzer)
+      }
+      ++docCounter
+    } else if (specificExtractor.streamDocs) {
+      if (fs.existsSync(join(outDir, 'meta', relPath))) {
+        continue
+      }
+      console.log(tolog)
+      let inputStr = args.part === 'chtyvo' ? filePath : fs.readFileSync(filePath, 'utf8')
+      let i = 0
+      for (let doc of specificExtractor.streamDocs(inputStr)) {
+        let docId = join(relPath, zerofill(i++, 4))
+        processDoc(doc, outDir, docId, analyzer)
+      }
+      docCounter += i
+    } else {
+      throw new Error(`No extractor for ${args.part}`)
+    }
+  }
 }
 
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
@@ -95,73 +78,11 @@ function globInforming(inputRoot: string, inputGlob = '**/*') {
 }
 
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-async function doUdpipeStage(args: Args) {
-  let outDir = getOutDir(args)
-  let inputRoot = join(outDir, 'para')
-
-  let udpipe = new UdpipeApiClient(args.udpipeUrl)
-  let runner = new AsyncTaskRunner()
-
-  let paraFiles = globInforming(inputRoot)
-  for (let [i, paraPath] of paraFiles.entries()) {
-    let basePath = trimExtension(path.relative(inputRoot, paraPath))
-    let conlluPath = join(outDir, 'conllu', `${basePath}.conllu`)
-
-    if (!fs.existsSync(conlluPath)) {
-      await runner.startRunning(async () => {
-        console.log(`udpiped ${i} docs (${toFloorPercent(i, paraFiles.length)}%), doing ${paraPath}`)
-
-        let paragraphs = parseJsonFileSync(paraPath)
-        let conllu = await udpipe.tokTagPlaintext(paragraphs2UdpipeInput(paragraphs))
-        writeFileSyncMkdirp(conlluPath, conllu)
-      })
-    }
-  }
-}
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-function doVerticalStage(args: Args) {
-  let outDir = getOutDir(args)
-  let inputRoot = join(outDir, 'conllu')
-
-  let conlluFiles = globInforming(inputRoot)
-  for (let [i, conlluPath] of conlluFiles.entries()) {
-
-    let relativePath = path.relative(inputRoot, conlluPath)
-    relativePath = trimExtension(relativePath)
-    let outPath = join(outDir, 'vertial', `${relativePath}.vrt`)
-    if (fs.existsSync(outPath)) {
-      continue
-    }
-    console.log(`verted ${i} docs (${toFloorPercent(i, conlluFiles.length)}%), doing ${conlluPath}`)
-
-    let metaPath = join(outDir, 'meta', `${relativePath}.json`)
-    let meta = parseJsonFileSync(metaPath)
-    let conlluStr = fs.readFileSync(conlluPath, 'utf8')
-    let vrtLines = conlluStrAndMeta2vertical(conlluStr, { meta })
-    writeFileSyncMkdirp(outPath, mu(vrtLines).join('\n', true))
-  }
-}
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-function paragraphs2UdpipeInput(paragraphs: Array<string>) {
-  return paragraphs.map(x => x.replace(/\u0301/g, '')).join('\n\n')
-}
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 interface Args {
-  stage: 'extract' | 'udpipe' | '4vec' | 'vertical'
   workspace: string
   part: string
-  mitei?: string
-
-  out?: string
   inputGlob: string
   inputRoot: string
-  udpipeUrl: string
-
-  checkUkr?: boolean
-  checkDate?: boolean
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/corpus-pipeline/extractors/twitter.ts
+++ src/corpus-pipeline/extractors/twitter.ts
@@ -8,111 +8,6 @@ import * as minimist from 'minimist'
 
 
 
-////////////////////////////////////////////////////////////////////////////////
-export class TwitterStreamingDocExtracor implements StreamingExtractor {
-  private reader = new JsonObjectLogReader()
-  private seenIds = new Set<string>()
-
-  private totalTweets = 0
-
-  constructor() {
-    this.reader.setIgnoreErrors()
-  }
-
-  feed(line: string) {
-    let obj = this.reader.feed(line)
-    if (!obj) {
-      return
-    }
-    ++this.totalTweets
-
-    let tweet = parseTweet(obj)
-    if (tweet.lang !== 'uk') {
-      return
-    }
-
-    let {
-      id,
-      text,
-      isTruncated,
-      url,
-      login,
-      createdAt,
-    } = tweet
-
-    let paragraphs = text.split(/(?:\s*\n+\s*)+/g)
-
-    if (isTruncated) {
-      paragraphs.pop()
-    }
-
-    if (!paragraphs.length) {
-      return
-    }
-
-    if (this.seenIds.has(id)) {
-      return
-    }
-
-    this.seenIds.add(id)
-
-    let author = nameFromLoginAtDomain(login, 'twitter.com')
-
-    return {
-      paragraphs,
-      title: '',  // todo
-      source: 'Твітер',
-      url,
-      author,
-      date: toSortableDatetime(createdAt),
-    }
-  }
-
-  getStats() {
-    return {
-      totalTweets: this.totalTweets,
-      numOriginal: this.seenIds.size
-    }
-  }
-}
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-function parseTweet(tweet) {
-  let cur = tweet
-  if (cur.retweeted_status) {
-    cur = cur.retweeted_status
-  }
-
-  let id = cur.id_str as string
-  let url = `https://twitter.com/statuses/${id}`
-  let login = cur.user.screen_name
-  let name = cur.user.name
-  let text: string = cur.extended_tweet && cur.extended_tweet.full_text
-    || cur.text
-  let createdAt = new Date(cur.created_at)
-
-  text = text.replace(/(\s*https:\/\/t.co\/[\w]+)+$/g, '')
-  // next 3 only:
-  text = text.replace(/&gt;/g, '>')
-  text = text.replace(/&lt;/g, '<')
-  text = text.replace(/&amp;/g, '&')
-  text = text.trim()
-
-  // todo: replace hrefs https://twitter.com/statuses/1000738462897704961
-
-
-  return {
-    id,
-    url,
-    text,
-    login,
-    name,
-    lang: cur.lang as string,
-    createdAt,
-    isTruncated: cur.truncated,
-  }
-}
-
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 class JsonObjectLogReader {
   private buf = ''
@@ -147,6 +42,127 @@ class JsonObjectLogReader {
 }
 
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function parseTweetObservation(tweet) {
+  let observedAt = new Date(tweet.created_at)
+
+  let cur = tweet
+  if (cur.retweeted_status) {
+    cur = cur.retweeted_status
+  }
+
+  let id = cur.id_str as string
+  let login = cur.user.screen_name
+  let name = cur.user.name
+  let isReposted = cur.extended_tweet
+  let text: string = isReposted && cur.extended_tweet.full_text
+    || cur.text
+  let createdAt = new Date(cur.created_at)
+
+  text = text.replace(/(\s*https:\/\/t.co\/[\w]+)+$/g, '')
+  // next 3 only:
+  text = text.replace(/&gt;/g, '>')
+  text = text.replace(/&lt;/g, '<')
+  text = text.replace(/&amp;/g, '&')
+
+  text = text.trim()
+
+  // todo: replace hrefs https://twitter.com/statuses/1000738462897704961
+
+  return {
+    observedAt,
+    isReposted,
+    id,
+    text,
+    login,
+    name,
+    lang: cur.lang as string,
+    createdAt,
+    isTruncated: cur.truncated,
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+export class TwitterStreamingDocExtracor implements StreamingExtractor {
+  private reader = new JsonObjectLogReader()
+  private seenIds = new Set<string>()
+
+  private totalTweets = 0
+
+  constructor() {
+    this.reader.setIgnoreErrors()
+  }
+
+  feed(line: string) {
+    let obj = this.reader.feed(line)
+    if (!obj) {
+      return
+    }
+    ++this.totalTweets
+
+    let tweet = parseTweetObservation(obj)
+    if (tweet.lang !== 'uk') {
+      return
+    }
+
+    let {
+      id,
+      text,
+      isTruncated,
+      login,
+      createdAt,
+      lang,
+    } = tweet
+
+    let paragraphs = text.split(/(?:\s*\n+\s*)+/g)
+
+    if (isTruncated) {
+      paragraphs.pop()
+    }
+
+    if (!paragraphs.length) {
+      return
+    }
+
+    if (this.seenIds.has(id)) {
+      return
+    }
+
+    this.seenIds.add(id)
+
+    if (twitterSpecificFilter(lang)) {
+      return
+    }
+
+    let author = nameFromLoginAtDomain(login, 'twitter.com')
+
+    return {
+      paragraphs,
+      title: '',  // todo
+      source: 'Твітер',
+      url: `https://twitter.com/statuses/${id}`,
+      author,
+      date: toSortableDatetime(createdAt),
+    }
+  }
+
+  getStats() {
+    return {
+      totalTweets: this.totalTweets,
+      numOriginal: this.seenIds.size
+    }
+  }
+}
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function twitterSpecificFilter(lang: string) {
+  if (lang !== 'uk') {
+    return true
+  }
+
+  return false
+}
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 async function main() {
   const args = minimist(process.argv.slice(2)) as any
 
@@ -161,7 +177,7 @@ async function main() {
     let doc = extractor.feed(line)
     if (doc) {
       // processDoc(doc, '', '', analyzer)
-      // console.log(doc)
+      console.log(doc)
     }
   })
   console.error(extractor.getStats())
